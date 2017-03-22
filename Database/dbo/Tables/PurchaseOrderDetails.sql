e52dde4943b0b9330ff6ef20ad6ae46944dcab13
Robert <rfleckner@my.apsu.edu>
2017-03-22 16:23:39 -0500

    Added more tables

---
+++ Database/dbo/Tables/PurchaseOrderDetails.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[PurchaseOrderDetails] (
+    [PurchaseOrderDetails_Id] INT          IDENTITY (1, 1) NOT NULL,
+    [Purchase Order Number]   INT          NOT NULL,
+    [Product]                 VARCHAR (50) NOT NULL,
+    [Quantity]                INT          NOT NULL,
+    [Unit Price]              MONEY        NOT NULL,
+    [Extended Price]          MONEY        NOT NULL,
+    [Date Received]           DATE         NOT NULL,
+    [Status]                  VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([PurchaseOrderDetails_Id] ASC),
+    CONSTRAINT [FK_PurchaseOrderDetails_ToTable] FOREIGN KEY ([Purchase Order Number]) REFERENCES [dbo].[PurchaseOrders] ([PurchaseOrders_Id])
+);
+
