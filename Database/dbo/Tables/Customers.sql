e8c338ce4f00a09b4707acc8eb1a04635ef058ef
Robert <rfleckner@my.apsu.edu>
2017-03-22 15:59:09 -0500

    Added tables

---
+++ Database/dbo/Tables/Customers.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [Customer_Id]    INT          IDENTITY (1, 1) NOT NULL,
+    [Company]        VARCHAR (50) NOT NULL,
+    [Last Name]      VARCHAR (50) NOT NULL,
+    [First Name]     VARCHAR (50) NOT NULL,
+    [Job Title]      VARCHAR (50) NOT NULL,
+    [Phone Number]   VARCHAR (50) NOT NULL,
+    [Address]        VARCHAR (50) NOT NULL,
+    [City]           VARCHAR (50) NOT NULL,
+    [State/Province] VARCHAR (50) NOT NULL,
+    [Zip]            INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Customer_Id] ASC)
+);
+
---
+++ Database/dbo/Tables/Product.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [Product_Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [Product Code] VARCHAR (20) NOT NULL,
+    [Product Name] NCHAR (10)   NOT NULL,
+    [Unit Cost]    MONEY        NOT NULL,
+    [List Price]   MONEY        NOT NULL,
+    [Discontinue]  BIT          NOT NULL,
+    [Category]     VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Product_Id] ASC)
+);
+
