c4bd19a3a42330224a46f0a0edff23f8729b06e9
Robert <rfleckner@my.apsu.edu>
2017-04-06 15:39:46 -0500

    Inventory table: Added Product_Id foreign key

--- Database/dbo/Tables/Inventory.sql
+++ Database/dbo/Tables/Inventory.sql
@@ -1,12 +1,16 @@
 ﻿CREATE TABLE [dbo].[Inventory] (
     [Inventory_Id]     INT          IDENTITY (1, 1) NOT NULL,
+    [Product_Id]       INT          NULL,
     [Product]          VARCHAR (50) NOT NULL,
     [On_Hand]          INT          NOT NULL,
     [Reorder_Level]    INT          NOT NULL,
     [Reorder_Quantity] INT          NOT NULL,
     [On_Order]         INT          NOT NULL,
-    PRIMARY KEY CLUSTERED ([Inventory_Id] ASC)
+    PRIMARY KEY CLUSTERED ([Inventory_Id] ASC),
+    CONSTRAINT [FK_Inventory_ToTable] FOREIGN KEY ([Product_Id]) REFERENCES [dbo].[Product] ([Product_Id])
 );
+
+
 
 
 
