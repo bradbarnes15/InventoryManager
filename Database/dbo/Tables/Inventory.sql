09f8c3cba0cbe6b2921f6dd6d4473bdbec3c3f6d
Robert <rfleckner@my.apsu.edu>
2017-04-07 10:43:33 -0500

    Inventory table: changed foreign key to not allow nulls

--- Database/dbo/Tables/Inventory.sql
+++ Database/dbo/Tables/Inventory.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Inventory] (
     [Inventory_Id]     INT          IDENTITY (1, 1) NOT NULL,
-    [Product_Id]       INT          NULL,
+    [Product_Id]       INT          NOT NULL,
     [Product]          VARCHAR (50) NOT NULL,
     [On_Hand]          INT          NOT NULL,
     [Reorder_Level]    INT          NOT NULL,
@@ -9,6 +9,8 @@
     PRIMARY KEY CLUSTERED ([Inventory_Id] ASC),
     CONSTRAINT [FK_Inventory_ToTable] FOREIGN KEY ([Product_Id]) REFERENCES [dbo].[Product] ([Product_Id])
 );
+
+
 
 
 
