e55dab739c5037a36f1f81da0e4171ce37a8028d
Robert <rfleckner@my.apsu.edu>
2017-04-11 15:34:07 -0500

    Inventory class: Added Get method

--- InventoryManagerLib/Inventory.cs
+++ InventoryManagerLib/Inventory.cs
@@ -16,25 +16,27 @@ public class Inventory : DBConnection
     private int On_Order { get; set; }
 
 
-    public Inventory(string Product,string product_Location, int On_Hand, int Reorder_Level, int Reorder_Quantity)
+    public Inventory(string Product, string Product_Location, int Product_Id, int On_Hand, int Reorder_Level, int Reorder_Quantity)
     {
         this.Product          = Product;
-        this.Product_Location = product_Location;
+        this.Product_Id       = Product_Id;
+        this.Product_Location = Product_Location;
         this.On_Hand          = On_Hand;
         this.Reorder_Level    = Reorder_Level;
         this.Reorder_Quantity = Reorder_Quantity;
         On_Order = 0;
     }
     
-    private Inventory(int Inventory_Id, string product_Location, int On_Hand, int Reorder_Level, int Reorder_Quantity)
+    private Inventory(int Inventory_Id, string Product, int Product_Id, string Product_Location, int On_Hand, int Reorder_Level, int Reorder_quantity, int On_Order)
     {
         this.Inventory_Id     = Inventory_Id;
         this.Product          = Product;
-        this.Product_Location = product_Location;
+        this.Product_Id       = Product_Id;
+        this.Product_Location = Product_Location;
         this.On_Hand          = On_Hand;
         this.Reorder_Level    = Reorder_Level;
         this.Reorder_Quantity = Reorder_Quantity;
-        On_Order = 0;
+        this.On_Order = On_Order;
     }
 
 
@@ -84,6 +86,81 @@ public class Inventory : DBConnection
             }
         }
     }
+
+
+    public static Inventory Get(int IdValue)
+    {
+        using (SqlConnection conn = new SqlConnection())
+        {
+            conn.ConnectionString = DBConnection.CONNECTION_STRING;
+            conn.Open();
+
+            string sql = "SELECT Inventory_Id, Product, Product_Id, Product_Location, On_Hand, Reorder_Level, Reorder_Quantity, On_Order"
+                       + "FROM Inventory"
+                       + "WHERE Inventory_Id = Inventory_Id";
+
+            SqlCommand command = new SqlCommand(sql, conn);
+            command.Parameters.AddWithValue("Inventory_Id", IdValue);
+
+            using (SqlDataReader read = command.ExecuteReader())
+            {
+                if (read.HasRows)
+                {
+                    read.Read();
+
+                    Inventory inv = new Inventory(read.GetInt32(0),
+                                                  read.GetString(2),
+                                                  read.GetInt32(1),
+                                                  read.GetString(3),
+                                                  read.GetInt32(4),
+                                                  read.GetInt32(5),
+                                                  read.GetInt32(6),
+                                                  read.GetInt32(7));
+                    return inv;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+
+
+    public static List<Inventory> GetAll()
+    {
+        using (SqlConnection conn = new SqlConnection())
+        {
+            conn.ConnectionString = DBConnection.CONNECTION_STRING;
+            conn.Open();
+
+            string sql = "SELECT Inventory_Id, Product, Product_Id, Product_Location, On_Hand, Reorder_Level, Reorder_Quantity, On_Order"
+                       + "FROM Inventory";
+
+            SqlCommand command = new SqlCommand(sql, conn);
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                List<Inventory> invList = new List<Inventory>();
+
+                while (reader.Read())
+                {
+                    Inventory item = new Inventory(reader.GetInt32(0),
+                                                  reader.GetString(2),
+                                                  reader.GetInt32(1),
+                                                  reader.GetString(3),
+                                                  reader.GetInt32(4),
+                                                  reader.GetInt32(5),
+                                                  reader.GetInt32(6),
+                                                  reader.GetInt32(7));
+
+                    invList.Add(item);
+                }
+
+                return invList;
+            }
+        }
+    }
+
 
 
     public override string ToString()
