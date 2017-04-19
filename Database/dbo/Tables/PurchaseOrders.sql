37e8c060092b893887f8e5afd06f636b9d97848b
Robert <rfleckner@my.apsu.edu>
2017-04-19 10:41:33 -0500

    PurchaseOrders table: Updated field names Purchase Orders class: Created constructors and Save method

--- Database/dbo/Tables/PurchaseOrders.sql
+++ Database/dbo/Tables/PurchaseOrders.sql
@@ -1,14 +1,16 @@
 ﻿CREATE TABLE [dbo].[PurchaseOrders] (
     [PurchaseOrders_Id] INT          IDENTITY (1, 1) NOT NULL,
-    [Order Date]        DATE         NOT NULL,
-    [Created By]        VARCHAR (50) NOT NULL,
-    [Created Date]      DATE         NOT NULL,
-    [Shipping Fee]      MONEY        NOT NULL,
-    [Taxes]             MONEY        NOT NULL,
-    [Payment Date]      DATE         NOT NULL,
-    [Payment Amount]    MONEY        NOT NULL,
-    [Order Subtotal]    MONEY        NOT NULL,
-    [Order Total]       MONEY        NOT NULL,
+    [Order_Date]        DATE         NOT NULL,
+    [Created_By]        VARCHAR (50) NOT NULL,
+    [Created_Date]      DATE         NOT NULL,
+    [Shipping_Fee]      FLOAT (53)   NOT NULL,
+    [Tax]               FLOAT (53)   NOT NULL,
+    [Payment_Date]      DATE         NOT NULL,
+    [Payment_Amount]    FLOAT (53)   NOT NULL,
+    [Order_Subtotal]    FLOAT (53)   NOT NULL,
+    [Order_Total]       FLOAT (53)   NOT NULL,
     PRIMARY KEY CLUSTERED ([PurchaseOrders_Id] ASC)
 );
+
+
 
--- InventoryManagerLib/PurchaseOrder.cs
+++ InventoryManagerLib/PurchaseOrder.cs
@@ -6,64 +6,104 @@
 //------------------------------------------------------------------------------
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 
 public class PurchaseOrder : DBConnection
 {
-	private int purchaseOrderDetailId
-	{
-		get;
-		set;
-	}
+	private int      PurchaseOrders_Id   { get; set; }
+    private DateTime Order_Date          { get; set; }
+	private string   Created_By          { get; set; }
+	private DateTime Created_Date        { get; set; }
+	private double   Shipping_Fee        { get; set; }
+	private double   Tax                 { get; set; }
+	private DateTime Payment_Date        { get; set; }
+	private double   Payment_Amount      { get; set; }
+    private double   Order_Subtotal      { get; set; }
+	private double   Order_Total         { get; set; }
 
-	private string orderDate
-	{
-		get;
-		set;
-	}
 
-	private string createdBy
-	{
-		get;
-		set;
-	}
+    public PurchaseOrder(DateTime Order_Date, string Created_By, DateTime Created_Date,double Shipping_Fee, double Tax, DateTime Payment_Date,double Payment_Amount, double Order_Subtotal, double Order_Total)
+    {
+        this.PurchaseOrders_Id = -1;
+        this.Order_Date     = Order_Date;
+        this.Created_By     = Created_By;
+        this.Created_Date   = Created_Date;
+        this.Shipping_Fee   = Shipping_Fee;
+        this.Tax            = Tax;
+        this.Payment_Date   = Payment_Date;
+        this.Payment_Amount = Payment_Amount;
+        this.Order_Subtotal = Order_Subtotal;
+        this.Order_Total    = Order_Total;
+    }
 
-	private string createdDate
-	{
-		get;
-		set;
-	}
+    private PurchaseOrder(int PurchaseOrders_Id, DateTime Order_Date, string Created_By, DateTime Created_Date, double Shipping_Fee, double Tax, DateTime Payment_Date, double Payment_Amount, double Order_Subtotal, double Order_Total)
+    {
+        this.PurchaseOrders_Id = PurchaseOrders_Id;
+        this.Order_Date = Order_Date;
+        this.Created_By = Created_By;
+        this.Created_Date = Created_Date;
+        this.Shipping_Fee = Shipping_Fee;
+        this.Tax = Tax;
+        this.Payment_Date = Payment_Date;
+        this.Payment_Amount = Payment_Amount;
+        this.Order_Subtotal = Order_Subtotal;
+        this.Order_Total = Order_Total;
+    }
 
-	private Double shippingFees
-	{
-		get;
-		set;
-	}
 
-	private Double taxes
-	{
-		get;
-		set;
-	}
 
-	private string paymentDate
-	{
-		get;
-		set;
-	}
+    public void Save()
+    {
+        using (SqlConnection conn = new SqlConnection())
+        {
+            conn.ConnectionString = DBConnection.CONNECTION_STRING;
+            conn.Open();
 
-	private Double paymentAmount
-	{
-		get;
-		set;
-	}
+            string sql;
 
-	private Double orderTotal
-	{
-		get;
-		set;
-	}
+            if (PurchaseOrders_Id == -1)
+            {
+                sql = "INSERT INTO PurchaseOrders(Order_Date, Created_By, Created_Date, Shipping_Fee, Tax, Payment_Date, Payment_Amount, Order_Subtotal, Order_Total) "
+                    + "Values(@Order_Date, @Created_By, @Created_Date, @Shipping_Fee, @Tax, @Payment_Date, @Payment_Amount, @Order_Subtotal, @Order_Total) "
+                    + "SELECT CAST (scope_identity() as int)";
+            }
+            else
+            {
+                sql = "UPDATE PurchaseOrders SET "
+                    + "Order_Date = @Order_Date, Created_By = @Created_By, Created_Date = @Created_Date, Shipping_Fee = @Shipping_Fee, Tax = @Tax, Payment_Date = @Payment_Date, Payment_Amount = @Payment_Amount, Order_Subtotal = @Order_Subtotal, Order_Total = @Order_Total "
+                    + "WHERE PurchaseOrder_Id = @PurchaseOrder_Id ";
+            }
+
+            SqlCommand command = new SqlCommand(sql, conn);
+
+            command.Parameters.AddWithValue("Order_Date", Order_Date);
+            command.Parameters.AddWithValue("Created_By", Created_By);
+            command.Parameters.AddWithValue("Created_Date", Created_Date);
+            command.Parameters.AddWithValue("Shipping_Fee", Shipping_Fee);
+            command.Parameters.AddWithValue("Tax", Tax);
+            command.Parameters.AddWithValue("Payment_Date", Payment_Date);
+            command.Parameters.AddWithValue("Payment_Amount", Payment_Amount);
+            command.Parameters.AddWithValue("Order_Subtotal", Order_Subtotal);
+            command.Parameters.AddWithValue("Order_Total", Order_Total);
+
+            if (PurchaseOrders_Id == -1)
+            {
+                PurchaseOrders_Id = (int)command.ExecuteScalar();
+            }
+            else
+            {
+                command.Parameters.AddWithValue("PurchaseOrders_Id", PurchaseOrders_Id);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public PurchaseOrder Get(int Purchase)
+    {
+
+    }
 
     public override string ToString()
     {
