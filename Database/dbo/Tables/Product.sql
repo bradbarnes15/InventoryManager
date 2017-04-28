b8328d0e9e70cc59a7051a798401ab4ff7430f4c
Bob <rfleckner@my.apsu.edu>
2017-04-28 14:48:37 -0500

    Updated Product_Name in table to hold 50 characters

--- Database/dbo/Tables/Product.sql
+++ Database/dbo/Tables/Product.sql
@@ -1,13 +1,15 @@
 ﻿CREATE TABLE [dbo].[Product] (
     [Product_Id]   INT          IDENTITY (1, 1) NOT NULL,
     [Product_Code] VARCHAR (20) NOT NULL,
-    [Product_Name] NCHAR (10)   NOT NULL,
+    [Product_Name] VARCHAR (50) NOT NULL,
     [Unit_Cost]    FLOAT (53)   NOT NULL,
     [List_Price]   FLOAT (53)   NOT NULL,
     [Discontinue]  BIT          DEFAULT ((0)) NOT NULL,
     [Category]     VARCHAR (50) NOT NULL,
     PRIMARY KEY CLUSTERED ([Product_Id] ASC)
 );
+
+
 
 
 
