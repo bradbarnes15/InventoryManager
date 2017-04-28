4164769b5f05a750d33eaa70f0889cd26e604881
Bob <rfleckner@my.apsu.edu>
2017-04-28 15:41:16 -0500

    Updates in Inventory table and class. Added script to add Products to product table

--- Database/dbo/Tables/Inventory.sql
+++ Database/dbo/Tables/Inventory.sql
@@ -1,15 +1,16 @@
 ﻿CREATE TABLE [dbo].[Inventory] (
     [Inventory_Id]     INT          IDENTITY (1, 1) NOT NULL,
-    [Product_Id]       INT          NOT NULL,
+    [Product_Code]     VARCHAR (50) NOT NULL,
     [Product]          VARCHAR (50) NOT NULL,
     [Product_Location] VARCHAR (50) NOT NULL,
     [On_Hand]          INT          NOT NULL,
     [Reorder_Level]    INT          NOT NULL,
     [Reorder_Quantity] INT          NOT NULL,
     [On_Order]         INT          NOT NULL,
-    PRIMARY KEY CLUSTERED ([Inventory_Id] ASC),
-    CONSTRAINT [FK_Inventory_ToTable] FOREIGN KEY ([Product_Id]) REFERENCES [dbo].[Product] ([Product_Id])
+    PRIMARY KEY CLUSTERED ([Inventory_Id] ASC)
 );
+
+
 
 
 
