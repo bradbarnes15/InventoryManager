d4c1178c747ed58321e8c993f6cd1652c5157255
Bob <rfleckner@my.apsu.edu>
2017-04-29 13:09:19 -0500

    Added script to fill OrderStatus table

---
+++ Database/dbo/Scripts/dbo.OrderStatus.data.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT [dbo].[OrderStatus] ON
+INSERT INTO [dbo].[OrderStatus] ([OrderStatus_Id], [Status_Text]) VALUES (1, N'New')
+INSERT INTO [dbo].[OrderStatus] ([OrderStatus_Id], [Status_Text]) VALUES (2, N'Invoiced')
+INSERT INTO [dbo].[OrderStatus] ([OrderStatus_Id], [Status_Text]) VALUES (3, N'Shipped')
+INSERT INTO [dbo].[OrderStatus] ([OrderStatus_Id], [Status_Text]) VALUES (4, N'Completed')
+SET IDENTITY_INSERT [dbo].[OrderStatus] OFF
