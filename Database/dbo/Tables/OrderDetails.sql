03e8aff0ef50136439eb08120021ad52bdeda314
Bob <rfleckner@my.apsu.edu>
2017-04-29 13:12:57 -0500

    Removed the unnecessary status column

--- Database/dbo/Tables/OrderDetails.sql
+++ Database/dbo/Tables/OrderDetails.sql
@@ -5,10 +5,11 @@
     [Quantity]        INT          NOT NULL,
     [Unit_Price]      FLOAT (53)   NOT NULL,
     [Extended_Price]  FLOAT (53)   NOT NULL,
-    [Status]          NCHAR (10)   NOT NULL,
     PRIMARY KEY CLUSTERED ([OrderDetails_Id] ASC),
     CONSTRAINT [FK_OrderDetails_ToTable] FOREIGN KEY ([Order_Id]) REFERENCES [dbo].[Orders] ([Order_Id])
 );
+
+
 
 
 
