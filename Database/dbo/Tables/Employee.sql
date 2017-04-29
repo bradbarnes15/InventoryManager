e5c75fe8b1bfae445064cfd8a8a9fb0eafb05e19
Bob <rfleckner@my.apsu.edu>
2017-04-29 16:17:05 -0500

    Updating scripts

--- Database/dbo/Tables/Employee.sql
+++ Database/dbo/Tables/Employee.sql
@@ -2,7 +2,7 @@
     [employee_id]        INT          IDENTITY (1, 1) NOT NULL,
     [first_name]         VARCHAR (50) NOT NULL,
     [last_name]          VARCHAR (50) NOT NULL,
-    [username]           NCHAR (10)   NOT NULL,
+    [username]           NCHAR (25)   NOT NULL,
     [encrypted_password] TEXT         NOT NULL,
     PRIMARY KEY CLUSTERED ([employee_id] ASC)
 );
