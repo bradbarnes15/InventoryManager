9e66da5cb14cf303c7e4db32131582733f44702a
brad <brad@DESKTOP-OS5QU8K>
2017-04-30 16:51:54 -0500

    added listview to the receving /create-order

--- Manager/CreateOrder.xaml.cs
+++ Manager/CreateOrder.xaml.cs
@@ -21,12 +21,44 @@ namespace Manager
     public partial class CreateOrder : Window
     {
         List<Product> list = new List<Product>();
+        double totalForOrder = 0;
         Employee k;
+        
         public CreateOrder(Employee x)
         {
             InitializeComponent();
             comboBox.ItemsSource = Product.GetAll();
             k = x;
+
+            // Add columns
+            var gridView = new GridView();
+            this.listView.View = gridView;
+            gridView.Columns.Add(new GridViewColumn 
+            {
+                Width = 75,
+                Header = "Product",
+                DisplayMemberBinding = new Binding("Product")
+                
+            });
+            gridView.Columns.Add(new GridViewColumn
+            {
+                Width = 75,
+                Header = "Quanitity",
+                DisplayMemberBinding = new Binding("Quanitity")
+            });
+            gridView.Columns.Add(new GridViewColumn
+            {
+                Width = 75,
+                Header = "Price",
+                DisplayMemberBinding = new Binding("Price")
+            });
+            gridView.Columns.Add(new GridViewColumn
+            {
+                Width=75,
+                Header = "Total",
+                DisplayMemberBinding = new Binding("Total")
+            });
+
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
@@ -38,6 +70,17 @@ namespace Manager
             window.Show();
         }
 
+        public class MyItem
+        {
+            public string Product { get; set; }
+
+            public int Quanitity { get; set; }
+
+            public double Price { get; set; }
+
+            public double Total { get; set; }
+        }
+
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             string value = comboBox.SelectedItem.ToString();
@@ -47,9 +90,16 @@ namespace Manager
             int newValue;
             if (Int32.TryParse(textBox.Text, out newValue))
             {
-                listBox.Items.Add("Product : " + x.Product_Name+ ""  + newValue);
+                listBox.Items.Add("Product : " + x.Product_Name+ " "  + newValue + " $" + x.List_Price + " " + (newValue*x.List_Price) );
             }
+            totalForOrder +=  (newValue* x.List_Price);
+            textBox1.Text = totalForOrder.ToString();
 
+            //  listView.Items.Add("Product : " + x.Product_Name + " " + newValue + " $" + x.List_Price + " " + (newValue * x.List_Price));
+            //listView.Items[count] = ("Product : " + x.Product_Name + " " + newValue + " $" + x.List_Price + " " + (newValue * x.List_Price) );
+            //count += 1;
+
+            listView.Items.Add(new MyItem { Product = x.Product_Name, Quanitity = newValue, Price = x.List_Price, Total = (Convert.ToDouble(newValue) * x.List_Price) });
 
             /*
             if ()
@@ -87,6 +137,11 @@ namespace Manager
         }
 
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
